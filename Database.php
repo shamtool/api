93be8fec84a7d65c68f16a87e74d74381787e1c1
Crollie Rollz <crollierollz@gmail.com>
2021-06-12 21:26:54 +0800

    update

---
+++ Database.php
@@ -0,0 +1,28 @@
+  
+<?php
+
+/**
+ * Represents a connection to the shamtool database.
+ */
+class STDatabase extends PDO
+{
+    private static ?STDatabase $instance = null;
+
+    public function __construct()
+    {
+        try {
+            parent::__construct(sprintf("mysql:host=%s;dbname=%s", $_ENV['DB_HOST'], $_ENV['DB_NAME']), $_ENV['DB_USER'], $_ENV['DB_PASS']);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance(): STDatabase
+    {
+        if (self::$instance == null) {
+            self::$instance = new STDatabase();
+        }
+        return self::$instance;
+    }
+}
--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
+// Primitive includes!
 require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS']);
+$dotenv->load();
+
+// Normal includes
+require __DIR__ . '/Database.php';
+$db = STDatabase::getInstance();
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
 
 // Define routes
 $router->get('/', function() {
-    echo 'Hello worlds';
+    global $db;
+    echo sprintf('Hello worlds! There was been currently %s recorded over the databased.',
+        $db->query("SELECT COUNT(*) FROM divmapInfo"));
 });
 
 // Run it!
